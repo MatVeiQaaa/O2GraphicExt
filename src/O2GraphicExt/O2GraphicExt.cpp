d59e68f516c9893d02e1b97af501544986c76618
mtvqaaa <matthehard@gmail.com>
2023-10-08 21:38:46 -0700

    Feature to edit combo digits spacing and positioning on X axis.

--- src/O2GraphicExt/O2GraphicExt.cpp
+++ src/O2GraphicExt/O2GraphicExt.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <thread>
 #include <psapi.h>
+#include <math.h>
 
 #include "helpers.hpp"
 
@@ -22,6 +23,8 @@ inline MH_STATUS MH_CreateHookEx(LPVOID pTarget, LPVOID pDetour, T** ppOriginal)
 {
 	return MH_CreateHook(pTarget, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
 }
+
+uintptr_t hOtwo = NULL;
 
 int* previousState = NULL;
 int* currentState = NULL;
@@ -86,6 +89,11 @@ scaleOjt ScaleOjt = NULL;
 typedef uintptr_t(__thiscall* loadSceneElement)(DWORD* buffer, DWORD idc, ResDetails* resDetails);
 loadSceneElement LoadSceneElement = NULL;
 
+int comboCentrePoint = 72;
+int comboDigitWidth = 44;
+int comboDigitsSpacing = 0;
+bool comboSmallNumberCorrections = true;
+
 uintptr_t __fastcall OnLoadSceneElement(DWORD* buffer, DWORD edx, DWORD idc, ResDetails* resDetails)
 {
 	uintptr_t result = LoadSceneElement(buffer, idc, resDetails);
@@ -137,6 +145,18 @@ uintptr_t __fastcall OnLoadSceneElement(DWORD* buffer, DWORD edx, DWORD idc, Res
 			{"Y", resource->data->ojt->YScale},
 			{"Multiplier", resource->data->ojt->scaleAllRelative}
 		};
+
+		if (strncmp(resDetails->name, "Note_ComboNum", 13) == 0)
+		{
+			element[resDetails->name]["Extra"] =
+			{
+				{"CentrePoint", 72},
+				{"DigitWidth", 44},
+				{"DigitsSpacing", 0},
+				{"SmallNumberCorrections", true}
+			};
+		}
+
 		json finalJson;
 		cfgInFile.open("O2GraphicExt.json");
 		finalJson = json::parse(cfgInFile);
@@ -158,15 +178,58 @@ uintptr_t __fastcall OnLoadSceneElement(DWORD* buffer, DWORD edx, DWORD idc, Res
 			resource->data->ojt->scaleAllRelative = config[resDetails->name]["Scale"]["Multiplier"];
 			ScaleOjt(resource->data->ojt, resource->data->ojt->scaleAllRelative);
 		}
+		if (strncmp(resDetails->name, "Note_ComboNum", 13) == 0)
+		{
+			cfgInFile.open("O2GraphicExt.json");
+			config = json::parse(cfgInFile);
+			comboCentrePoint = config[resDetails->name]["Extra"]["CentrePoint"];
+			comboDigitWidth = config[resDetails->name]["Extra"]["DigitWidth"];
+			comboDigitsSpacing = config[resDetails->name]["Extra"]["DigitsSpacing"];
+			comboSmallNumberCorrections = config[resDetails->name]["Extra"]["SmallNumberCorrections"];
+			cfgInFile.close();
+		}
 	}
 
 	return result;
 }
 
+typedef int(__thiscall* drawCombo)(DWORD* pThis, int combo, int x, int y);
+drawCombo DrawCombo = NULL;
+
+int __fastcall OnDrawCombo(DWORD* pThis, DWORD edx, int combo, int x, int y)
+{
+	int selectedMap = *(int*)(hOtwo + 0x24D88);
+	char* offsetTens = (char*)(hOtwo + 0x287F6);
+	char* offsetHundreds = (char*)(hOtwo + 0x287EB);
+	int* offsetThousands = (int*)(hOtwo + 0x287E5);
+	char* oneCorrection = (char*)(hOtwo + 0x2886A);
+	char* twoCorrectionOpcode = (char*)(hOtwo + 0x2887F);
+	int gap = -comboDigitWidth - comboDigitsSpacing;
+	int digitsCount = combo > 9 ? (int)log10((double)combo) + 1 : 1;
+	x = comboCentrePoint + (comboDigitWidth + comboDigitsSpacing) / 2 * digitsCount;
+	*offsetTens = gap;
+	*offsetHundreds = gap * 2;
+	*offsetThousands = gap * 3;
+	/*0x2886A - increase digit X position for number 1 as 1 - byte unsigned int. (Default = 5)
+	0x2887F - increase digit X position by 1 for number 2 as opcode 0x42 (inc edx), can replace for 0x90 to disable.*/
+	if (comboSmallNumberCorrections)
+	{
+		*oneCorrection = 5;
+		*twoCorrectionOpcode = 0x42;
+	}
+	else
+	{
+		*oneCorrection = 0;
+		*twoCorrectionOpcode = 0x90;
+	}
+
+	return DrawCombo(pThis, combo, x, y);
+}
+
 int O2GraphicExt::init(HMODULE hModule)
 {
 	Sleep(5000);
-	uintptr_t hOtwo = (uintptr_t)GetModuleHandle("OTwo.exe");
+	hOtwo = (uintptr_t)GetModuleHandle("OTwo.exe");
 	MODULEINFO modOtwo;
 	GetModuleInformation(GetCurrentProcess(), (HMODULE)hOtwo, &modOtwo, sizeof(MODULEINFO));
 	using namespace sigmatch_literals;
@@ -178,6 +241,10 @@ int O2GraphicExt::init(HMODULE hModule)
 	previousState = (int*)FollowPointers(hOtwo, { 0x1C8884, 0x4C });
 	currentState = (int*)FollowPointers(hOtwo, { 0x1C8884, 0x50 });
 
+	DrawCombo = (drawCombo)((uintptr_t)hOtwo + 0x028780);
+	DWORD curProtection = 0;
+	BOOL hResult = VirtualProtect((void*)(hOtwo + 0x2886A), 160, PAGE_EXECUTE_READWRITE, &curProtection);
+
 	if (MH_Initialize() != MH_OK)
 	{
 		Logger("Couldn't init MinHook");
@@ -187,6 +254,12 @@ int O2GraphicExt::init(HMODULE hModule)
 	if (MH_CreateHookEx((LPVOID)LoadSceneElement, &OnLoadSceneElement, &LoadSceneElement) != MH_OK)
 	{
 		Logger("Couldn't hook LoadSceneElement");
+		return 1;
+	}
+
+	if (MH_CreateHookEx((LPVOID)DrawCombo, &OnDrawCombo, &DrawCombo) != MH_OK)
+	{
+		Logger("Couldn't hook DrawCombo");
 		return 1;
 	}
 
